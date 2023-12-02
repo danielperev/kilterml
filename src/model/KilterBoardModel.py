b40f89bb300fbac6217658e4fa75a1ac8d9b14d0
danielperev <daniel.perevalov@gmail.com>
2023-12-02 11:37:37 -0500

    Add initial model and data helper (placements)
    Add base images and data in respective folders

---
+++ src/model/KilterBoardModel.py
@@ -0,0 +1,129 @@
+from placements import Placements
+import torch
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+placements = Placements()
+# placements.plot_matrix_placements(placements.all_matrix_climbs, plot_size=1)
+
+#A climb matrix is a 36 x 36 matrix with 1-2 start holds, more than 2 middle holds, optional footholds, and at least one finish hold
+
+#Model that will classify the difficulty of a climb based off it's matrix (hold placements)
+class BoardClassificationModel(torch.nn.Module):
+    def __init__(self):
+        ...
+
+#Model that will generate a climb matrix based off of X parameters (grade, angle, etc.)
+class BoardGenerationModel(torch.nn.Module):
+    def __init__(self, vocab_size, emb_size, hidden_size, num_classes):
+        super(BoardGenerationModel, self).__init__()
+        self.vocab_size = vocab_size
+        self.emb_size = emb_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.emb = torch.nn.Embedding(vocab_size, emb_size)
+        self.rnn = torch.nn.RNN(emb_size, hidden_size, batch_first=True)
+        self.fc = torch.nn.Linear(hidden_size * 2, num_classes)
+
+    def forward(self, X):
+        # Look up the embedding
+        wordemb = self.emb(X)
+        # Forward propagate the RNN
+        h, out = self.rnn(wordemb)
+        # combine the hidden features computed from *each* time step of
+        # the RNN. we do this by 
+        features = torch.cat([torch.amax(h, dim=1),
+                              torch.mean(h, dim=1)], dim=-1)
+        # Compute the final prediction
+        z = self.fc(features)
+        return z
+    
+def accuracy(model, dataset, max=1000):
+    """
+    Estimate the accuracy of `model` over the `dataset`.
+    We will take the **most probable class**
+    as the class predicted by the model.
+
+    Parameters:
+        `model`   - An object of class nn.Module
+        `dataset` - A dataset of the same type as `train_data`.
+        `max`     - The max number of samples to use to estimate 
+                    model accuracy
+
+    Returns: a floating-point value between 0 and 1.
+    """
+
+    correct, total = 0, 0
+    dataloader = DataLoader(dataset,
+                            batch_size=1,  # use batch size 1 to prevent padding
+                            collate_fn=collate_batch)
+    for i, (x, t) in enumerate(dataloader):
+        z = model(x)
+        y = torch.argmax(z, axis=1)
+        correct += int(torch.sum(t == y))
+        total   += 1
+        if i >= max:
+            break
+    return correct / total
+
+# accuracy(model, train_data_indices)
+
+def train_model(model,                # an instance of MLPModel
+                train_data,           # training data
+                val_data,             # validation data
+                learning_rate=0.001,
+                batch_size=100,
+                num_epochs=10,
+                plot_every=50,        # how often (in # iterations) to track metrics
+                plot=True):           # whether to plot the training curve
+    train_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               collate_fn=collate_batch,
+                                               shuffle=True) # reshuffle minibatches every epoch
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # these lists will be used to track the training progress
+    # and to plot the training curve
+    iters, train_loss, train_acc, val_acc = [], [], [], []
+    iter_count = 0 # count the number of iterations that has passed
+
+    try:
+        for e in range(num_epochs):
+            for i, (texts, labels) in enumerate(train_loader):
+                z = model(texts) # TODO
+                loss = criterion(z, labels.long())# TODO
+
+                loss.backward() # propagate the gradients
+                optimizer.step() # update the parameters
+                optimizer.zero_grad() # clean up accumualted gradients
+
+                iter_count += 1
+                if iter_count % plot_every == 0:
+                    iters.append(iter_count)
+                    ta = accuracy(model, train_data)
+                    va = accuracy(model, val_data)
+                    train_loss.append(float(loss))
+                    train_acc.append(ta)
+                    val_acc.append(va)
+                    print(iter_count, "Loss:", float(loss), "Train Acc:", ta, "Val Acc:", va)
+    finally:
+        # This try/finally block is to display the training curve
+        # even if training is interrupted
+        if plot:
+            plt.figure()
+            plt.plot(iters[:len(train_loss)], train_loss)
+            plt.title("Loss over iterations")
+            plt.xlabel("Iterations")
+            plt.ylabel("Loss")
+
+            plt.figure()
+            plt.plot(iters[:len(train_acc)], train_acc)
+            plt.plot(iters[:len(val_acc)], val_acc)
+            plt.title("Accuracy over iterations")
+            plt.xlabel("Iterations")
+            plt.ylabel("Accuracy")
+            plt.legend(["Train", "Validation"])
+
+# model = MyRNN(len(vocab), 128, 64, 2)
+
---
+++ src/placements_helper/placements.py
@@ -0,0 +1,95 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import random
+
+class Placements:
+    def __init__(self, filepath:str):
+        self.file_path = filepath
+        self.all_data = {}
+        self.all_placements = {}
+        self.all_matrix_climbs = {}
+        self.all_impossible_placements = {}
+        self.DEFAULT_MATRIX_SIZE=(36,35)
+        self.DEFAULT_MATRIX={}
+        self.setup_placements()
+        self.setup_default_matrix()
+        self.setup_matrices()
+        
+        self._default_board_cmap = ListedColormap(["#2e2d2e", "#2e2d2e", "#00dd00", "#00ffff", "#ffa600", "#ff00ff"])
+        self._empty_board_cmap = ListedColormap(["#ff0000", "#26ff00"])
+
+    def clean_hold(self, hold: dict) -> dict:
+        if "ledPosition" in hold:
+                del hold["ledPosition"]
+
+        match hold["type"]:
+            case "START": #green
+                hold["value"] = 1
+            case "MIDDLE": #blue
+                hold["value"] = 2
+            case "FEET-ONLY": #orange
+                hold["value"] = 3
+            case "FINISH": #pink
+                hold["value"] = 4
+        del hold["type"]
+
+        hold['x'] = max(hold['x']-1,0)
+        return hold
+    
+    def setup_placements(self):
+        self.all_data = json.load(open(self.file_path + "\\all_data.json", "r", encoding="utf8"))
+        self.all_impossible_placements = json.load(open(self.file_path + "\\impossible_holds.json", "r", encoding="utf8"))["placements"]
+        for climb in self.all_data:
+            self.all_placements[climb["name"]] = [self.clean_hold(hold) for hold in climb["placements"] if "type" in hold]
+
+    def setup_default_matrix(self):
+        matrix = np.zeros(self.DEFAULT_MATRIX_SIZE, dtype=np.uint8)
+        matrix = np.hstack((matrix,np.full((matrix.shape[0],1), -1)))
+        for coords in self.all_impossible_placements:
+            matrix[coords["y"]][coords["x"]] = -1
+        self.DEFAULT_MATRIX=matrix
+        
+    def setup_matrices(self):
+        for name,placements in self.all_placements.items():
+            self.all_matrix_climbs[name] = self.placements_to_matrix(placements)
+
+    def placements_to_matrix(self, placements: list[dict]) -> list[list]:
+        climb_matrix = self.DEFAULT_MATRIX.copy()
+
+        for hold in placements:
+            climb_matrix[hold['y']][hold['x']] = hold['value']
+        
+        return climb_matrix
+
+    def all_placements_one_board(self, placements_dict: dict[str, list[dict]], matrix: list[list]) -> list[list]:
+        for _,placements in placements_dict.items():
+            for hold in placements:
+                if matrix[hold['y']][hold['x']] == 0:
+                    matrix[hold['y']][hold['x']] = hold['value']
+        
+        return matrix
+
+    def sample_random_climbs(all_climbs: dict[str, list[list]], sample_number = 9):
+        return {k:v for k,v in random.sample(list(all_climbs.items()), sample_number)}
+
+    def plot_matrix_placements(self, matrix_placements: dict[str, list[list]], plot_size = 3):
+        fig, axes = plt.subplots(plot_size, plot_size, figsize=(36,36))
+        if plot_size > 1:
+            for i,ax in enumerate(axes.flat):
+                ax.tick_params(axis='both', which='major', labelsize=7)
+                ax.set_title([key for index,key in enumerate(matrix_placements.keys()) if index == i][0], fontsize=9)
+                ax.matshow([value for index,value in enumerate(matrix_placements.values()) if index == i][0], cmap=self._default_board_cmap)
+        else:
+            axes.set_title(list(matrix_placements.keys())[0], fontsize=16)
+            axes.matshow(list(matrix_placements.values())[0], cmap=self._default_board_cmap)
+        fig.set_size_inches((10, 10))
+        plt.subplots_adjust(hspace=0.25, wspace=0.0)
+        plt.show()
+#-----
+
+# plot_matrix_placements(sample_random_climbs(all_matrix_climbs))
+# plot_matrix_placements(all_placements_one_board({"all": all_placements}, DEFAULT_MATRIX.copy()), plot_size=1)
+# plot_matrix_placements({"all": DEFAULT_MATRIX}, plot_size=1, color_map=empty_board_cmap)
+
